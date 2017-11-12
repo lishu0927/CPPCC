dc2d2455ab623f2935ab525e534e27cc407c6ba2
coderZhengYaTeng <zhengyt@mpen.com.cn>
2017-11-12 15:48:45 +0800

    查询

--- controllers/members/members.js
+++ controllers/members/members.js
@@ -2,34 +2,34 @@ var createRequest = require("../../config/settings");
 
 module.exports = {
     init: function (req, res) {
-        var keyword = req.query.keyword||'';
+        var keyword = req.query.keyword || '';
         var data = {};
-        createRequest(res,data,"/ZxApi/m2_01.ashx",function () {
+        createRequest(res, data, "/ZxApi/m2_01.ashx", function () {
             body = JSON.parse(body);
-            var group_list=body.group_list
+            var group_list = body.group_list
             res.render('members/members-md',
                 {
                     title: '委员名单',
-                    keyword:keyword,
+                    keyword: keyword,
                     group_list: group_list
                 }
             );
         });
     },
     search: function (req, res) {
-        var keyword = req.query.keyword||'';
+        var keyword = req.query.keyword || '';
         var data = {keyword: keyword};
-        createRequest(res,data,"/ZxApi/m2_02.ashx",function () {
+        createRequest(res, data, "/ZxApi/m2_02.ashx", function () {
             body = JSON.parse(body);
             console.log(keyword);
-            var group_list=body.group_list
+            var group_list = body.group_list
             /* var tian_list=body.tian_list
             var article_list=body.article_list*/
             res.render('members/members-search',
                 {
                     title: '查询结果',
                     group_list: group_list,
-                    keyword:keyword,
+                    keyword: keyword,
                 }
             );
         })
@@ -37,9 +37,9 @@ module.exports = {
     detail: function (req, res) {
         var id = req.query.id;
         var data = {user_id: id};
-        createRequest(res,data,"/ZxApi/m2_03.ashx",function () {
+        createRequest(res, data, "/ZxApi/m2_03.ashx", function () {
             body = JSON.parse(body);
-            var member=body.user_info
+            var member = body.user_info
             res.render('members/members-detail',
                 {
                     title: '政协委员信息表',
@@ -50,9 +50,9 @@ module.exports = {
     },
     edit: function (req, res) {
         var data = {};
-        createRequest(res,data,"/ZxApi/m2_01.ashx",function () {
+        createRequest(res, data, "/ZxApi/m2_01.ashx", function () {
             body = JSON.parse(body);
-            var group_list=body.group_list
+            var group_list = body.group_list
             console.log(group_list)
             res.render('members/members-edit',
                 {
@@ -63,28 +63,23 @@ module.exports = {
         });
     },
     editGroup: function (req, res) {
-        var data = {};
-        createRequest(res,data,"/ZxApi/m2_01.ashx",function () {
-            body = JSON.parse(body);
-            console.log(body)
-            res.render('members/edit-group',
-                {
-                    title: '编辑组',
-                    group_list: group_list
-                }
-            );
-        });
+        var id = req.query.id;
+        res.render('members/edit-group',
+            {
+                title: '编辑组',
+                groupid: id
+            });
     },
     test: function (req, res) {
-        var keyword = req.query.keyword||'';
+        var keyword = req.query.keyword || '';
         var data = {keyword: keyword};
-        createRequest(res,data,"/ZxApi/m2_02.ashx",function () {
+        createRequest(res, data, "/ZxApi/m2_02.ashx", function () {
             body = JSON.parse(body);
             console.log(keyword)
             res.json(
                 {
                     title: '查询结果',
-                    keyword:keyword,
+                    keyword: keyword,
                 }
             );
         })
