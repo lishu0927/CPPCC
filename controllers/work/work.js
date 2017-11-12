8e83af0503b548932697b3a88cb061e427676f6f
lishu0927 <1830077485@qq.com>
2017-11-12 17:14:11 +0800

    政协工作

---
+++ controllers/work/work.js
@@ -0,0 +1,24 @@
+var createRequest = require("../../config/settings");
+
+module.exports = {
+    search: function (req, res) {
+        var keyword = req.query.keyword||'';
+        var data = {keyword: keyword};
+        createRequest(res,data,"/ZxApi/m1_04.ashx",function () {
+            console.log(body);
+            body = JSON.parse(body);
+            var group_list=body.group_list
+            var tian_list=body.tian_list
+            var article_list=body.article_list
+            res.render('search/search',
+                {
+                    title: '查询结果',
+                    group_list: group_list,
+                    tian_list: tian_list,
+                    article_list: article_list,
+                    keyword:keyword
+                }
+            );
+        })
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,38 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index', {title: '政协要闻'});
 });
-router.get('/members', function (req, res, next) {
+router.get('/members', function (req, res) {
     res.render('members/members', {title: '政协委员',keyword:'', type: 2});
 });
-router.get('/work', function (req, res, next) {
+router.get('/work', function (req, res) {
     res.render('work/work', {title: '政协工作',keyword:'', type: 3});
 });
-router.get('/mine', function (req, res, next) {
+router.get('/mine', function (req, res) {
     res.render('mine/mine', {title: '我', keyword:'',type: 4});
 });
-router.get('/login', function (req, res, next) {
+router.get('/login', function (req, res) {
     res.render('login/login', {title: '登录'});
 });
-router.get('/editMember', function (req, res, next) {
+router.get('/editMember', function (req, res) {
     res.render('members/edit-member', {title: '编辑成员信息'});
+});
+router.get('/workInfo', function (req, res) {
+    res.render('work/work-info', {title: '政协简介'});
+});
+router.get('/workOrder', function (req, res) {
+    res.render('work/work-order', {title: '政协领导'});
+});
+router.get('/workFiles', function (req, res) {
+    res.render('work/work-files', {title: '政协文件'});
+});
+router.get('/fileDetail', function (req, res) {
+    res.render('work/file-detail', {title: '政协文件'});
+});
+router.get('/workNotices', function (req, res) {
+    res.render('work/work-notices', {title: '通知'});
+});
+router.get('/noticeDetail', function (req, res) {
+    res.render('work/notice-detail', {title: '通知'});
 });
 
 module.exports = router;
